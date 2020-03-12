71fd7ae6faa5c58e78a97f8a360e0e8904753564
venkatesh <venkateshmuthu86@gmail.com>
2020-03-12 14:56:02 +0530

    Commit
    Login And Registation validation completed

---
+++ Registration/WebApplication1/Controllers/RegistrationController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+using WebApplication1.Views;
+
+namespace WebApplication1.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private RegistrationEntities db = new RegistrationEntities();
+
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.users.ToList());
+        }
+
+
+        //Return Register view
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveRegisterDetails(Register registerDetails)
+        {
+            //We check if the model state is valid or not. We have used DataAnnotation attributes.
+            //If any form value fails the DataAnnotation validation the model state becomes invalid.
+            if (ModelState.IsValid)
+            {
+                //create database context using Entity framework 
+                using (var databaseContext = new RegistrationEntities())
+                {
+                    //If the model state is valid i.e. the form values passed the validation then we are storing the User's details in DB.
+                    user reglog = new user();
+
+                    //Save all details in RegitserUser object
+
+                    reglog.FirstName = registerDetails.FirstName;
+                    reglog.LastName = registerDetails.LastName;
+                    reglog.Email = registerDetails.Email;
+                    reglog.Password = registerDetails.Password;
+
+
+                    //Calling the SaveDetails method which saves the details.
+                    databaseContext.users.Add(reglog);
+                    databaseContext.SaveChanges();
+                }
+
+                ViewBag.Message = "User Details Saved";
+                return View("Register");
+            }
+            else
+            {
+
+                //If the validation fails, we are returning the model object with errors to the view, which will display the error messages.
+                return View("Register", registerDetails);
+            }
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //The login form is posted to this method.
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            //Checking the state of model passed as parameter.
+            if (ModelState.IsValid)
+            {
+
+                //Validating the user, whether the user is valid or not.
+                var isValidUser = IsValidUser(model);
+
+                //If user is valid & present in database, we are redirecting it to Welcome page.
+                if (isValidUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, false);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    //If the username and password combination is not present in DB then error message is shown.
+                    ModelState.AddModelError("Failure", "Wrong Username and password combination !");
+                    return View();
+                }
+            }
+            else
+            {
+                //If model state is not valid, the model with error message is returned to the View.
+                return View(model);
+            }
+        }
+
+        //function to check if User is valid or not
+        public user IsValidUser(LoginViewModel model)
+        {
+            using (var dataContext = new RegistrationEntities())
+            {
+                //Retireving the user details from DB based on username and password enetered by user.
+                user user = dataContext.users.Where(query => query.Email.Equals(model.Email) && query.Password.Equals(model.Password)).SingleOrDefault();
+                //If user is present, then true is returned.
+                if (user == null)
+                    return null;
+                //If user is not present false is returned.
+                else
+                    return user;
+            }
+        }
+
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon(); // it will clear the session at the end of request
+            return RedirectToAction("Index");
+        }
+    }
+}
